814957218a5ebbf83311200a941a55506dc52730
BrunoVictorino <azrbruno@gmail.com>
2023-02-06 18:43:51 -0300

    alteração

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
-import java.util.Locale;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double largura = sc.nextDouble();
-        double comprimento = sc.nextDouble();
-        double metroQuadrado = sc.nextDouble();
+        int N = sc.nextInt();
+        int soma = 0;
 
-        double area = largura * comprimento;
-        double preco = area * metroQuadrado;
+        for (int i=0; i<N; i++){
+            int x = sc.nextInt();
+            soma += x;
+        }
 
-        System.out.printf("AREA = %.2f%n", area);
-        System.out.printf("PRECO = %.2f%n", preco);
+        System.out.println(soma);
 
         sc.close();
     }
